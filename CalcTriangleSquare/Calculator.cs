5906cdeb164f13b39ebd16e6703ac6001dc01785
0WhyMe0 <wolodymyrpelekh@gmail.com>
2024-05-28 14:29:53 +0200

    Add project files.

---
+++ CalcTriangleSquare/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcTriangleSquare
+{
+    internal class Calculator
+    {
+        public double CalctriangleSquare(double ab, double bc, double ac)
+        {
+            double p = (ab + bc + ac) / 2;
+
+            return Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+
+        public double CalctriangleSquare(double a, double h)
+        {
+            return 0.5 * a * h;
+        }
+    }
+}
---
+++ CalcTriangleSquare/Program.cs
@@ -0,0 +1,17 @@
+﻿using CalcTriangleSquare;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        double square1 = calculator.CalctriangleSquare(10, 20);
+        double square2 = calculator.CalctriangleSquare(16, 25, 36);
+
+        double roundeNumberSquare1 = Math.Round(square1, 2);
+        double roundeNumberSquare2 = Math.Round(square2, 2);
+
+        Console.WriteLine($"Square1: {roundeNumberSquare1}. Square2: {roundeNumberSquare2}");
+    }
+}
